8ae48ae3d78be221313293b58f0c57ad0371c76a
Manuel Moreno <manuel.moreno@televisatim.com>
2020-02-11 18:46:24 -0600

    ADD:Se crea migracion para categorias

--- database/migrations/2020_02_11_002131_create_categorias_table.php
+++ database/migrations/2020_02_11_002131_create_categorias_table.php
@@ -14,7 +14,20 @@ class CreateCategoriasTable extends Migration
     public function up()
     {
         Schema::create('categorias', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->unsigned();
+            $table->bigInteger('id_arbol_menu');
+            $table->unsignedBigInteger('id_usuario')->nullable();
+            $table->foreign('id_usuario')->references('id')->on('users')->onDelete('set null');
+            $table->string('titulo',100);
+            $table->text('descripcion');
+            $table->integer('ordenamiento');
+            $table->boolean('activo');
+            $table->string('pagina',70);
+            $table->string('componente', 100);
+            $table->boolean('alto_impacto_movil');
+            $table->boolean('alto_impacto_apple_tv');
+            $table->integer('ordenamiento_alto_impacto_movil');
+            $table->integer('ordenamiento_alto_impacto_apple_tv');
             $table->timestamps();
         });
     }
